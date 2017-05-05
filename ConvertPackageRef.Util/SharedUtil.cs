939d5fe6fba20cf4d3c354d64defbfa946d4434d
Jared Parsons <jaredpar@microsoft.com>
2017-05-05 09:56:50 -0700

    Util for converting PackageReference version
    The legacy project system doesn't support evaluated properties in Visual
    Studio.  Wrote a tool to convert the properties to the expanded
    versions.

---
+++ ConvertPackageRef.Util/SharedUtil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace ConvertPackageRef
+{
+    public static class SharedUtil
+    {
+        public static string MSBuildNamespaceUriRaw => "http://schemas.microsoft.com/developer/msbuild/2003";
+        public static Uri MSBuildNamespaceUri { get; } = new Uri(MSBuildNamespaceUriRaw);
+        public static XNamespace MSBuildNamespace { get; } = XNamespace.Get(MSBuildNamespaceUriRaw);
+        public static Encoding Encoding { get; } = Encoding.UTF8;
+
+        /// <summary>
+        /// NuGet package names are not case sensitive.
+        /// </summary>
+        public static readonly StringComparer NugetPackageNameComparer = StringComparer.OrdinalIgnoreCase;
+
+        /// <summary>
+        /// NuGet package versions case sensitivity is not documented anywhere that could be found.  Assuming
+        /// case insensitive for now.
+        /// </summary>
+        public static readonly StringComparer NugetPackageVersionComparer = StringComparer.OrdinalIgnoreCase;
+    }
+}
--- InlinePackageReferenceVersion/Program.cs
+++ InlinePackageReferenceVersion/Program.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using ConvertPackageRef;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
 
 namespace InlinePackageReferenceVersion
 {
@@ -10,7 +16,72 @@ namespace InlinePackageReferenceVersion
     {
         internal static void Main(string[] args)
         {
+            var map = ReadPackageVersionMap();
+            foreach (var solution in args)
+            {
+                foreach (var project in SolutionUtil.ParseProjects(solution))
+                {
+                    if (project.ProjectType != ProjectFileType.CSharp && project.ProjectType != ProjectFileType.Basic)
+                    {
+                        continue;
+                    }
 
+                    var filePath = Path.Combine(Path.GetDirectoryName(solution), project.RelativeFilePath);
+                    Convert(filePath, map);
+                }
+            }
         }
+
+        internal static void Convert(string projectFilePath, Dictionary<string, string> packageMap)
+        {
+            var doc = XDocument.Load(projectFilePath);
+            var manager = new XmlNamespaceManager(new NameTable());
+            manager.AddNamespace("mb", SharedUtil.MSBuildNamespaceUriRaw);
+            var tf = doc.XPathSelectElements("//mb:TargetFramework", manager).FirstOrDefault();
+            if (tf != null)
+            {
+                return;
+            }
+
+            Console.WriteLine($"Processing {Path.GetFileName(projectFilePath)}");
+
+            var versionName = SharedUtil.MSBuildNamespace.GetName("Version");
+            foreach (var packageRef in doc.XPathSelectElements("//mb:PackageReference", manager))
+            {
+                var version = packageRef.Element(versionName);
+                var value = version.Value.Trim();
+                if (value.StartsWith("$("))
+                {
+                    value = value.Substring(2, value.Length - 3);
+                    var rawVersion = packageMap[value];
+                    version.Value = rawVersion;
+                }
+            }
+
+            doc.Save(projectFilePath);
+        }
+
+        internal static Dictionary<string, string> ReadPackageVersionMap()
+        {
+            var packageVersionMap = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+            ReadPackageVersionMapCore(packageVersionMap, @"e:\code\roslyn\build\Targets\Packages.props");
+            ReadPackageVersionMapCore(packageVersionMap, @"e:\code\roslyn\build\Targets\FixedPackages.props");
+            return packageVersionMap;
+        }
+
+        internal static Dictionary<string, string> ReadPackageVersionMapCore(Dictionary<string, string> packageVersionMap, string path)
+        {
+            var doc = XDocument.Load(path);
+            var manager = new XmlNamespaceManager(new NameTable());
+            manager.AddNamespace("mb", SharedUtil.MSBuildNamespaceUriRaw);
+            var prop = doc.XPathSelectElements("//mb:PropertyGroup", manager).Single();
+            foreach (var element in prop.Elements())
+            {
+                packageVersionMap.Add(element.Name.LocalName, element.Value.Trim());
+            }
+
+            return packageVersionMap;
+        }
+
     }
 }
