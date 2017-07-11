bd857259e1e2a6daaf5c6a46c570737be6638bbb
Jared Parsons <jaredpparsons@gmail.com>
2017-07-11 12:49:55 -0700

    Remove ProjectGuid

--- QuickChange/Program.cs
+++ QuickChange/Program.cs
@@ -25,6 +25,7 @@ namespace QuickChange
         internal static void Convert(string baseDir, string solutionRelativePath)
         {
             var solutionFullPath = Path.Combine(baseDir, solutionRelativePath);
+            var solutionDir = Path.GetDirectoryName(solutionFullPath);
             foreach (var project in SolutionUtil.ParseProjects(solutionFullPath))
             {
                 if (project.ProjectType != ProjectFileType.CSharp && project.ProjectType != ProjectFileType.Basic)
@@ -33,11 +34,11 @@ namespace QuickChange
                 }
 
                 var filePath = Path.Combine(Path.GetDirectoryName(solutionFullPath), project.RelativeFilePath);
-                ConvertProject(filePath);
+                ConvertProject(filePath, solutionDir);
             }
         }
 
-        internal static void ConvertProject(string projectFullPath)
+        internal static void ConvertProject(string projectFullPath, string solutionDir)
         {
             var util = new ProjectUtil(projectFullPath);
             if (!util.IsNewSdk)
@@ -45,17 +46,26 @@ namespace QuickChange
                 return;
             }
 
+            if (projectFullPath.Contains("DeployCoreClr"))
+            {
+
+            }
 
             var doc = util.Document;
             var manager = new XmlNamespaceManager(new NameTable());
             manager.AddNamespace("mb", SharedUtil.MSBuildNamespaceUriRaw);
+            var changed = RemoveAll(doc, manager, "ProjectGuid");
+            changed |= CleanProjectReference(util, manager);
 
-            var toRemove = new[]
+            if (changed)
             {
-                "ProjectTypeGuids",
-                "TargetFrameworkProfile"
-            };
+                Console.WriteLine($"Processing {Path.GetFileName(projectFullPath)}");
+                doc.Save(projectFullPath);
+            }
+        }
 
+        internal static bool RemoveAll(XDocument doc, XmlNamespaceManager manager, params string[] toRemove)
+        {
             var found = false;
             foreach (var item in toRemove)
             {
@@ -67,11 +77,36 @@ namespace QuickChange
                 }
             }
 
-            if (found)
+            return found;
+        }
+
+        internal static bool CleanProjectReference(ProjectUtil util, XmlNamespaceManager manager)
+        {
+            var changed = false;
+            var projectDir = Path.GetDirectoryName(util.FilePath);
+            var doc = util.Document;
+            var elements = doc.XPathSelectElements($"//mb:ProjectReference", manager).ToList();
+            foreach (var element in elements)
             {
-                Console.WriteLine($"Processing {Path.GetFileName(projectFullPath)}");
-                doc.Save(projectFullPath);
+                var refRelativePath = element.Attribute("Include").Value;
+                var refFullPath = Path.Combine(projectDir, refRelativePath);
+                var refUtil = new ProjectUtil(refFullPath);
+                if (refUtil.IsNewSdk)
+                {
+                    element.Element(SharedUtil.MSBuildNamespace.GetName("Project"))?.Remove();
+                    element.Element(SharedUtil.MSBuildNamespace.GetName("Name"))?.Remove();
+                    if (!element.Elements().Any())
+                    {
+                        var newElem = new XElement(SharedUtil.MSBuildNamespace.GetName("ProjectReference"));
+                        newElem.Add(new XAttribute("Include", refRelativePath));
+                        element.ReplaceWith(newElem);
+                    }
+
+                    changed = true;
+                }
             }
+
+            return changed;
         }
     }
 }
